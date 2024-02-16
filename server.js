b78d5a72177d8edfac2e9d5600fec87f60d57337
Issam Bekkari <bekkariissam1999@gmail.com>
2024-02-16 06:40:01 +0100

    Update server.js
    Added some comments to understand the code even better

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express()
 mongoose.connect('mongodb://localhost/blogdb')
 
 app.set('view engine', 'ejs') // set the templating engine
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: false })) //setting up middleware to parse URL-encoded data and make it available in req.body. This is commonly used for processing form submissions in Express.js applications
+app.use(methodOverride('_method')) //override the form action method (to be able to patch, put or delete from the db, because a form action processor only provides the ability to post and get)
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({
